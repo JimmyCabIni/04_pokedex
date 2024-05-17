877604ac7eaceb935a40434aa837beebae370ef0
Jaime <jaimecaballero99@gmail.com>
2024-05-17 15:01:24 +0200

    Volvemos de detalles del pokemon a la lista de pokemons

--- src/app/pages/detail-pokemon/detail-pokemon.page.ts
+++ src/app/pages/detail-pokemon/detail-pokemon.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavParams} from "@ionic/angular";
+import {NavController, NavParams} from "@ionic/angular";
 import {Pokemon} from "../../models/pokemon";
 
 @Component({
@@ -7,18 +7,19 @@ import {Pokemon} from "../../models/pokemon";
   templateUrl: './detail-pokemon.page.html',
   styleUrls: ['./detail-pokemon.page.scss'],
 })
-export class DetailPokemonPage implements OnInit {
+export class DetailPokemonPage {
 
   public pokemon: Pokemon;
 
   constructor(
     private navParams: NavParams,
+    private navController: NavController,
   ) {
     this.pokemon = this.navParams.data["pokemon"];
     console.log(this.pokemon);
   }
 
-  ngOnInit() {
+  goBack() {
+    this.navController.pop();
   }
-
 }
