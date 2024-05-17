267a81371a1ed7af0a8a3846dddb85ab036ff3fc
Jaime <jaimecaballero99@gmail.com>
2024-05-17 13:20:58 +0200

    Obtenemos los pokemon con Capacitor

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {CapacitorHttp, HttpResponse} from "@capacitor/core";
+import {Pokemon} from "../models/pokemon";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,40 @@ export class PokemonService {
 
     if (url){
 
+      const options = {
+        url,
+        headers: {},
+        params: {}
+      }
+
+      return CapacitorHttp.get(options).then(async (response) => {
+        let pokemons: Pokemon[] = [];
+
+        console.log(response);
+        if (response.data) {
+          const resuls = response.data.results;
+          this.nextUrl = response.data.next;
+
+          const promises: Promise<HttpResponse>[] = [];
+
+          for (let index = 0; index < resuls.length; index++) {
+            const pokemon = resuls[index];
+            const urlPokemon = pokemon.url;
+            const options = {
+              url: urlPokemon,
+              headers: {},
+              params: {}
+            }
+            promises.push(CapacitorHttp.get(options));
+          }
+
+          await Promise.all(promises).then((responses) => {
+            console.log(responses)
+          })
+        }
+
+        return pokemons;
+      })
     }
     return null;
   }
